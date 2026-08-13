3033117f1c8a4c7f8fb5c36acb269cd5c320de39
thAnus <anthony.souto@estudante.ifms.edu.br>
2026-08-13 14:26:09 -0400

    edit: new date

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -4,6 +4,7 @@ const PORTA = 3000
 
 const server = http.createServer((req, res) => {
     console.log(`Requisicao recebida! ${req.method} ${req.url}`)
+    console.log(new Date().toISOString())
 
     res.statusCode = 201;
     res.setHeader ('Content-type', 'text/plain; charset=utf-8');
